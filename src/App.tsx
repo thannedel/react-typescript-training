8d3c045cdb1384add32d4107516038a929898548
thannedel <57347092+thannedel@users.noreply.github.com>
2021-04-14 08:11:50 +0200

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Navbar from "../src/components/common/Navbar";
+import Home from "../src/components/common/Home";
+import House from '../src/components/Congress/House'
+import { MemberProvider } from "./contexts/MemberContext";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MemberProvider>
+      <Router>
+      <div className="App">
+          <Navbar />
+          <div className="content">
+            <Switch>
+              <Route exact path="/">
+                 <Home />
+              </Route>
+            </Switch>
+            <Switch>
+              <Route path="/house">
+                 <House />
+              </Route>
+            </Switch>
+            
+            </div>
+        </div>
+        </Router>
+    </MemberProvider>
   );
 }
 
