c8e05271cbc49606e33db404229523574abaca76
thannedel <57347092+thannedel@users.noreply.github.com>
2021-04-14 09:56:50 +0200

    add reactstrap nav

---
+++ src/components/common/Navigation.tsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+const Navigation = () => {
+  
+    const [collapsed, setCollapsed] = useState(true);
+
+  const toggleNavbar = () => setCollapsed(!collapsed);
+  return(
+    <div>
+      <Navbar color="faded" light style={{backgroundColor: '#f1f1f1'}}>
+        <NavbarBrand tag={Link} to="/" className="mr-auto">Home</NavbarBrand>
+        <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+        <Collapse isOpen={!collapsed} navbar>
+          <Nav navbar>
+            <NavItem>
+              <NavLink tag={Link} to="/house/">Congress/House</NavLink>
+            </NavItem>
+            </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+export default Navigation;
+
+
+
+
+  
+
+ 
